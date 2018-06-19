e48e6192be025db58c1aed8ddbfe93624d8185eb
Shane Becker <veganstraightedge@gmail.com>
2018-06-18 19:58:01 -0700

    Refactor links in footer

---
+++ app/models/url.rb
@@ -0,0 +1,8 @@
+class URL
+  CONTACT_US_EMAIL = 'veganstraightedge@gmail.com'.freeze
+  SOCIAL_LINKS     = {
+    GitHub:   'https://github.com/ethiodotapp',
+    Twitter:  'https://twitter.com/ethiodotapp',
+    Intagram: 'https://instagram.com/ethiodotapp'
+  }.freeze
+end
