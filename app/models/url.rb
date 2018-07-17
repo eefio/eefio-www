b400b4abcdfb7def0b67fa528d5a6feaf2e16dbb
Shane Becker <veganstraightedge@gmail.com>
2018-07-16 22:48:12 -0700

    Add site footer styles

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,8 @@
 class URL
   CONTACT_US_EMAIL = 'veganstraightedge@gmail.com'.freeze
   SOCIAL_LINKS     = {
-    GitHub:   'https://github.com/eefiodotapp',
-    Twitter:  'https://twitter.com/eefiodotapp',
-    Intagram: 'https://instagram.com/eefiodotapp'
+    GitHub:   'https://github.com/eefio',
+    Twitter:  'https://twitter.com/EefioAPI',
+    Intagram: 'https://instagram.com/EefioAPI'
   }.freeze
 end
