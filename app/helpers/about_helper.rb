53f790758fd78d995ee6b9f8cecd2fe7c9a20ed4
Shane Becker <veganstraightedge@gmail.com>
2018-07-16 22:15:09 -0700

    Add homepage hero (responsive) styles

--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -1,6 +1,6 @@
 module AboutHelper
   def homepage_hero_background_image
-    filename = ('01'..'11').to_a.sample + '.jpg'
+    filename = "#{(1..9).to_a.sample}.jpg"
     path     = asset_url "homepage/backgrounds/#{filename}"
 
     "background-image: url(#{path});"
