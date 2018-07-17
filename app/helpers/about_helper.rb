fc34381be3d556be89bfe8a7c5e216e210321f35
Shane Becker <veganstraightedge@gmail.com>
2018-07-16 20:38:05 -0700

    Add Alright Sans fonts and CSS stack

--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -1,2 +1,9 @@
 module AboutHelper
+  def homepage_hero_background_image
+    image     = %w[05-2 05-3 06-1 07-1 07-2 07-3 08-1 08-2 08-3 13 16].sample + '.jpg'
+    image_url = asset_url "homepage/backgrounds/#{image}"
+
+    "background-image: url(#{image_url});"
+    nil
+  end
 end
