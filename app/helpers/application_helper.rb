93e2b9dc5900c37faf215b092a42337118ef98da
Shane Becker <veganstraightedge@gmail.com>
2019-01-19 18:33:50 -0800

    Add advisors to homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
   def body_id
     "id='#{@body_id}'".html_safe if @body_id.present?
   end
+
+  def link_to_heading_anchor id
+    content_tag :small, link_to('#', "##{id}", class: 'text-muted')
+  end
 end
